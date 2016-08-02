9f1ff26c5e40e9caf48f812554bbedab353a8929
DK-Coder <kb01020304@qq.com>
2016-08-02 14:18:27 +0800

    新增方法
    新增方法

--- Toolkits/DKSystemToolkit.h
+++ Toolkits/DKSystemToolkit.h
@@ -16,4 +16,39 @@
  *  @return 
  */
 + (float)systemVersion;
+
+/**
+ *  获取app的版本号
+ *
+ *  @return 
+ */
++ (float)appVersion;
+
+/**
+ *  获取app的名称
+ *
+ *  @return 
+ */
++ (NSString *)appName;
+
+/**
+ *  获取设备的机型，比如是iPhone或者iPod或者iPad
+ *
+ *  @return
+ */
++ (NSString *)deviceModel;
+
+/**
+ *  获取设备名称
+ *
+ *  @return
+ */
++ (NSString *)deviceName;
+
+/**
+ *  获取设备平台的名称，比如iPhone5或者iPhone6。（模拟器无效）
+ *
+ *  @return 
+ */
++ (NSString *)devicePlatform;
 @end
